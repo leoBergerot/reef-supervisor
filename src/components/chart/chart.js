7abf8c6551de83578e25cd94b9c8a32f8835e7ee
LeoBergerot <leo.bergerot@gmail.com>
2020-05-13 12:37:51 +0200

    scale date axis

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -11,10 +11,13 @@ import Typography from "@material-ui/core/Typography";
 import {MuiPickersContext} from "@material-ui/pickers";
 import {ArgumentAxis, Chart as ChartDevExpress, LineSeries, ValueAxis} from '@devexpress/dx-react-chart-material-ui';
 import Skeleton from "@material-ui/lab/Skeleton";
-import {ValueScale} from "@devexpress/dx-react-chart";
+import {ArgumentScale, ValueScale} from "@devexpress/dx-react-chart";
 import {DateRangePicker} from "../common/date-range-picker";
 import {appFetch, GET} from "../../utils/app-fetch";
 import {useTranslation} from "react-i18next";
+import {scaleTime} from 'd3-scale';
+import {timeDay, timeHour, timeMinute, timeMonth, timeSecond, timeWeek, timeYear} from "d3-time";
+import {timeFormatDefaultLocale} from "d3-time-format";
 
 const useStyle = makeStyles((theme) =>
     ({
@@ -39,7 +42,12 @@ const useStyle = makeStyles((theme) =>
             overflow: "scroll"
         },
         chart: {
-            height: "100% !important"
+            height: "100% !important",
+            "& #bottom-axis-container text": {
+                [theme.breakpoints.down("xs")]: {
+                    fontSize: '0.4rem'
+                },
+            }
         }
     }));
 
@@ -49,12 +57,42 @@ export const Chart = () => {
     const {type} = useContext(typeContext);
     const {getAuthToken} = useContext(authContext);
     const {setAlert} = useContext(alertContext);
-    const {t} = useTranslation();
+    const {i18n, t} = useTranslation();
 
     const [selectedDate, handleDateChange] = React.useState([moment().subtract(1, 'months'), moment()]);
     const [measures, setMeasures] = useState({data: [], loading: true});
 
     const classes = useStyle();
+
+    const timeLocal = timeFormatDefaultLocale(i18n.language.substring(0, 2) === 'fr' ?
+        {
+            dateTime: "%x, %X",
+            date: "%-d/%-m/%Y",
+            time: "%-I:%M:%S",
+            periods: ["Matin", "Après midi"],
+            days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+            shortDays: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+            months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"],
+            shortMonths: ["Jan", "Fév", "Mar", "Avr", "Mai", "Jui", "Jul", "Au", "Sep", "Oct", "Nov", "Dec"]
+        } : {
+            dateTime: "%x, %X",
+            date: "%-m/%-d/%Y",
+            time: "%-I:%M:%S %p",
+            periods: ["AM", "PM"],
+            days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+            shortDays: ["Sun", "Mon", "Mar", "Wed", "Thu", "Fri", "Sat"],
+            months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+            shortMonths: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        });
+
+    const formatMillisecond = timeLocal.format(".%L"),
+        formatSecond = timeLocal.format(":%S"),
+        formatMinute = timeLocal.format(i18n.language.substring(0, 2) === 'fr' ? "%H:%M" : "%I:%M"),
+        formatHour = timeLocal.format(i18n.language.substring(0, 2) === 'fr' ? "%Hh" : "%I%p"),
+        formatDay = timeLocal.format("%a %d"),
+        formatWeek = timeLocal.format("%b %d"),
+        formatMonth = timeLocal.format("%B"),
+        formatYear = timeLocal.format("%Y");
 
     useEffect(() => {
         getMeasures()
@@ -69,10 +107,13 @@ export const Chart = () => {
             getAuthToken(),
                 (result) => {
                     setMeasures({
-                        data: result.map((value) => ({
+                        data: result.map((value) => {
+                            let date = moment(value.createdAt);
+                            return ({
                             value: value.value,
-                            createdAt: moment(value.createdAt).format("DD/MM/YY HH:mm:ss")
-                        })),
+                                createdAt: new Date(date.format('YYYY'), date.format('MM') - 1, date.format('DD'), date.format('HH'), date.format('mm'))
+                            })
+                        }),
                         loading: false
                     });
                 },
@@ -82,6 +123,7 @@ export const Chart = () => {
             setAlert
         )
     };
+
     const getDomain = (domain) => {
         if (domain[0] / 10 < 1) {
             return [(domain[0] - 0.5) < 0 ? domain[0] : domain[0] - 0.5, domain[1] + 0.5]
@@ -94,6 +136,7 @@ export const Chart = () => {
         }
 
     };
+
     return (
         <Loading loading={type.loading}>
             <Paper variant="elevation" className={classes.root}>
@@ -110,8 +153,18 @@ export const Chart = () => {
                             data={measures.data}
                             className={classes.chart}
                         >
+                            <ArgumentScale factory={scaleTime}/>
                             <ValueScale modifyDomain={getDomain}/>
-                            <ArgumentAxis showLabels={false} showTick={false}/>
+                            <ArgumentAxis className={classes.argument} tickFormat={(scale) => (
+                                scale.tickFormat = (date) => (
+                                    timeSecond(date) < date ? formatMillisecond
+                                        : timeMinute(date) < date ? formatSecond
+                                        : timeHour(date) < date ? formatMinute
+                                            : timeDay(date) < date ? formatHour
+                                                : timeMonth(date) < date ? (timeWeek(date) < date ? formatDay : formatWeek)
+                                                    : timeYear(date) < date ? formatMonth
+                                                        : formatYear)(date)
+                            )}/>
                             <ValueAxis/>
                             <LineSeries valueField="value" argumentField="createdAt"/>
                         </ ChartDevExpress>
